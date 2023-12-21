fa4f2bf421f5a5d90212b7092b21bed8e71f6d3d
Pradeep Yadav <pradeepyadav8253@gmail.com>
2023-12-21 20:29:44 +0530

    first commit

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,6 +1,7 @@
-// A mock function to mimic making an async request for data
 export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
-  );
+  return new Promise(async (resolve) => {
+    const reponse = await fetch("http://localhost:8080");
+    const data = await reponse.json();
+    resolve({ data });
+  });
 }
---
+++ src/features/order/Order.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+export default function Order() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
